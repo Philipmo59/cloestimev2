ae1921330afcd96fc21edb5b77f0dafe34148ab7
philipmo <philipmo59@gmail.com>
2023-07-22 12:31:46 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import {Route, Switch} from "react-router-dom";
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route exact path ="/About">
+           <About />
+        </Route>
+        <Route exact path ="/Contact">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Contact(){
+    <div>
+        <h1>Hello Contact</h1>
+    </div>
+}
